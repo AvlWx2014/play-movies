dda6c9f6a7b51485e1b011d735c6877c0cbf58ec
Ryan Smith <avlwx2014@gmail.com>
2020-07-17 14:32:29 -0400

    Now that movie doesn't use custom IDs it doesn't need a custom codec. Remove all of that

--- app/Module.scala
+++ app/Module.scala
@@ -1,23 +1,17 @@
-import java.util.Date
-
-import codec.{DateCodec, MovieCodecProvider}
 import com.google.inject.{AbstractModule, Provides}
 import javax.inject.Singleton
 import net.codingwell.scalaguice.ScalaModule
-import org.bson.codecs.Codec
 import org.bson.codecs.configuration.CodecRegistries._
-import org.bson.codecs.configuration.{CodecProvider, CodecRegistry}
+import org.bson.codecs.configuration.CodecRegistry
+import org.mongodb.scala.bson.codecs.Macros
 import org.mongodb.scala.{MongoClient, MongoDatabase}
-import persistence.{MovieDao, MovieRepository}
+import persistence.{Movie, MovieDao, MovieRepository}
 import play.api.{Configuration, Environment}
 
 class Module(environment: Environment, configuration: Configuration) extends AbstractModule with ScalaModule {
 
   override def configure(): Unit = {
     bind[MovieRepository].to[MovieDao].in[Singleton]()
-//    bind[Codec[UUID]].to[UuidCodec]
-    bind[Codec[Date]].to[DateCodec]
-    bind[CodecProvider].to[MovieCodecProvider]
   }
 
   @Provides
@@ -28,13 +22,9 @@ class Module(environment: Environment, configuration: Configuration) extends Abs
   }
 
   @Provides
-  def provideCodeRegistry(dateCodec: Codec[Date], movieCodecProvider: CodecProvider): CodecRegistry = {
-//    val uuidCodec = new UuidCodec()
-//    val dateCodec = new DateCodec()
-//    val movieCodecProvider = new MovieCodecProvider()
+  def provideCodeRegistry(): CodecRegistry = {
     fromRegistries(
-      fromCodecs(dateCodec),
-      fromProviders(movieCodecProvider),
+      fromProviders(Macros.createCodecProvider(classOf[Movie])),
       MongoClient.DEFAULT_CODEC_REGISTRY
     )
   }
